f89d6b4ff1218aa879cc47f4e755bde430db8bc0
cientista-avogadro <cientistaavogadro2019@gmail.com>
2022-01-21 17:40:40 +0100

    Criação do controller Agendamento

---
+++ Consultorio/Controllers/AgendamentoController.cs
@@ -0,0 +1,43 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Consultorio.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+
+        List<Agendamento> agendamentos = new List<Agendamento>();
+        public AgendamentoController()
+        {
+            agendamentos.Add(new Agendamento 
+            { 
+                Id = 1, 
+                NomePaciente = "Cientista", 
+                Horario = new DateTime(2022, 01, 21) 
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(agendamentos);
+        }
+
+
+        [HttpGet("busca-agendamento/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var buscaSeleccionado = agendamentos.FirstOrDefault(x => x.Id == id);
+
+            return buscaSeleccionado != null
+                ? Ok(agendamentos)
+                : BadRequest("Erro ao buscar Agendamento");
+        }
+    }
+}
